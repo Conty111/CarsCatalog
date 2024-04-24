0c14687b6c1e8b518fa79fc5f3373fce5feb66b1
Dmitriy <mitj88@gmail.com>
2024-04-24 15:20:17 +0300

    initial commit

---
+++ internal/gateways/web/controllers/apiv1/base_controller.go
@@ -0,0 +1,8 @@
+package apiv1
+
+import "github.com/gin-gonic/gin"
+
+// Controller is an interface for HTTP controllers
+type Controller interface {
+	DefineRoutes(gin.IRouter)
+}
---
+++ internal/gateways/web/controllers/apiv1/swagger/controller.go
@@ -0,0 +1,31 @@
+package swagger
+
+//
+//import (
+//	"github.com/gin-gonic/gin"
+//	swaggerFiles "github.com/swaggo/files"     // swagger embed files
+//	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
+//
+//	//nolint: golint //reason: blank import because of swagger docs init
+//	//_ "github.com/wajox/gobase/api/web"
+//	"github.com/Conty111/internal/gateways/web/controllers/apiv1"
+//)
+//
+//var (
+//	_ apiv1.Controller = (*Controller)(nil)
+//)
+//
+//// Controller implements controller for swagger
+//type Controller struct {
+//	apiv1.BaseController
+//}
+//
+//// NewController create new instance for swagger controller
+//func NewController() *Controller {
+//	return &Controller{}
+//}
+//
+//// DefineRoutes adds swagger controller routes to the router
+//func (ctrl *Controller) DefineRoutes(r gin.IRouter) {
+//	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+//}
